f056c80cc82f8530b5e0982747eb7eacde6e96f4
Arthur <66388988+ssSamurai71@users.noreply.github.com>
2021-08-07 14:02:03 -0700

    removed changes in scene change

--- SceneChanger.cs
+++ SceneChanger.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChanger : Main_Loop
+public class SceneChanger : MonoBehaviour
 {
     public void swapScene(string sceneName)
     {
-        Save_stats();
         SceneManager.LoadScene(sceneName);
     }
 }
